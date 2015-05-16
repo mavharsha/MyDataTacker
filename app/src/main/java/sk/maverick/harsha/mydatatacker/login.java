ec1fbcaa80a51b0e967bf675af8a34bf30ae873d
mavharsha <sreeharsha.mamilla@gmail.com>
2015-05-15 17:19:34 -0700

    user homescreen

--- app/src/main/java/sk/maverick/harsha/mydatatacker/login.java
+++ app/src/main/java/sk/maverick/harsha/mydatatacker/login.java
@@ -13,7 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -28,7 +28,7 @@ public class login extends ActionBarActivity {
     TextView signup;
     String username, pass, line, current_device_number;
     TelephonyManager telephonyManager;
-
+    final String TAG = ".harsha.mydatatacker.login";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,24 +49,21 @@ public class login extends ActionBarActivity {
             @Override
             public void onClick(View v) {
 
-                 username = user.getText().toString();
-                 pass = password.getText().toString();
+                username = user.getText().toString();
+                pass = password.getText().toString();
 
 
-               if( regexValidator.validatePhoneNumber(username)==false)
-               {
-                   Toast.makeText(getApplicationContext(), "Please check the Phone number entered", Toast.LENGTH_SHORT).show();
-
-               }
-
-                if( regexValidator.validatePassword(pass)==false)
-                {
+                if (!regexValidator.validatePhoneNumber(username)) {
                     Toast.makeText(getApplicationContext(), "Please check the Phone number entered", Toast.LENGTH_SHORT).show();
 
                 }
 
-               if(regexValidator.validatePhoneNumber(username)==true & regexValidator.validatePassword(pass)==true )
-                {
+                if (!regexValidator.validatePassword(pass)) {
+                    Toast.makeText(getApplicationContext(), "Please check the Phone number entered", Toast.LENGTH_SHORT).show();
+
+                }
+
+                if (regexValidator.validatePhoneNumber(username) & regexValidator.validatePassword(pass)) {
                     new loginAsync().execute();
                 }
 
@@ -77,7 +74,6 @@ public class login extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(sk.maverick.harsha.mydatatacker.login.this, signUp.class));
-               // Log.v("Login","SignUP login");
             }
         });
 
@@ -86,7 +82,6 @@ public class login extends ActionBarActivity {
 
     @Override
     protected void onDestroy() {
-
         super.onDestroy();
         finish();
     }
@@ -124,8 +119,7 @@ public class login extends ActionBarActivity {
                 *  send user entered number(username), password (pass) and the current device phone number
                 * (current_device_number) as parameters in the URL
                 * */
-
-                 url = new URL ("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139");
+                url = new URL (new uri().getIp() +"Login/Authenticate/?phoneNo="+username+"&password="+pass+"&devicePhoneNo="+current_device_number);
 
             }catch (MalformedURLException e){
                 e.printStackTrace();
@@ -141,16 +135,11 @@ public class login extends ActionBarActivity {
                 http.connect();
 
                 /* Response */
-                if(http == null)
-                {
-                    Log.v("Async", "HTTP null response, response message :"+ http.getResponseMessage());
+                if(http.getResponseCode() == 200){
 
-                }else
-                {
                     InputStream in = http.getInputStream();
                     StringBuffer buffer = new StringBuffer();
                     BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
 
                     while ((line = reader.readLine()) != null) {
                         // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
@@ -158,24 +147,20 @@ public class login extends ActionBarActivity {
                         // buffer for debugging.
                         buffer.append(line + "\n");
                     }
-
                     line = buffer.toString();
 
                     Log.v("Async  response", "Response" + line);
 
                     /* Creating a json object of the response */
-                   // JSONObject obj = new JSONObject(line);
-
+                    // JSONObject obj = new JSONObject(line);
                     //String temp = obj.getString("country");
-                   // String temp2 = obj.getString("sunrise");
-
+                    // String temp2 = obj.getString("sunrise");
                     //Log.v("Asynnc Json", obj.getString("country") + "  " + obj.getString("sunrise"));
-
                     Log.v("Login Async", "http connect works " + http.getResponseMessage());
                 }
             }catch (Exception e){
                 e.printStackTrace();
-                Log.v("Login Async","Login Async HTTP Connection catch block");
+                Log.v(TAG,"Login Async HTTP Connection catch block");
             }
             return null;
         }
@@ -185,14 +170,17 @@ public class login extends ActionBarActivity {
         protected void onPostExecute(Long aLong) {
 
             /* If the response for the and the entered number and current phone number is same, owner */
-            startActivity(new Intent(sk.maverick.harsha.mydatatacker.login.this, homeScreen.class));
 
+            if(line.equalsIgnoreCase("success") & username.equalsIgnoreCase(current_device_number))
+
+            {
+                startActivity(new Intent(sk.maverick.harsha.mydatatacker.login.this, ownerHomeScreen.class));
+            }
+            else
+            {
+
+            }
         }
     }
-
-
-
-
-
 
 }
